a412d9b6451d44e07f961f6dd9402dfa96ad94a7
Aldous Roy <aldousroy007@gmail.com>
2024-04-16 22:22:08 +0530

    first commit

---
+++ clock.js
@@ -0,0 +1,26 @@
+const hour=document.getElementById("hour");
+const min=document.getElementById("minute");
+const sec=document.getElementById("second");
+const ampmEl=document.getElementById("ampm");
+function updateClock(){
+    let h=new Date().getHours();
+    let m=new Date().getMinutes();
+    let s=new Date().getSeconds();
+    let ampm="AM";
+    
+    if(h>12){
+        h=h-12;
+        ampm="PM";
+    }
+    h=h<10 ? "0"+h :h;
+    m=m<10 ? "0"+m :m;
+    s=s<10 ? "0"+s :s;
+hour.innerHTML=h;
+min.innerHTML=m;
+sec.innerHTML=s;
+ampmEl.innerHTML=ampm;
+setTimeout(()=>{
+    updateClock();
+},1000)
+}
+updateClock();
